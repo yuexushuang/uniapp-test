2d24e141464b008e52fc96a5df216d8c87e85ce4
yuexushuang <yuexushuang@163.com>
2021-04-15 18:04:44 +0800

    Update videoplay.vue
    增加视频播放失败弹窗判断

--- pages/test/videoplay/videoplay.vue
+++ pages/test/videoplay/videoplay.vue
@@ -15,20 +15,37 @@
 	export default {
 		data() {
 			return {
-				
+				isErrorModalShowed:false,
 			}
 		},
 		onReady: function (res) {
 			//通过API播放行为的时候需要创建videoContext
 			this.videoContext = uni.createVideoContext('myVideo');
+			//改变页面标题
+			uni.setNavigationBarTitle({
+			    title:'测试视频播放'
+			})
 		},
 		methods: {
 			videoErrorCallback: function(e) {
 				//播放中可能会出现多次错误，要避免多次弹窗
-				uni.showModal({
-					content: e.target.errMsg,
-					showCancel: false
-				})
+				if(this.isErrorModalShowed){
+					return;
+				}else{
+					this.isErrorModalShowed=true;
+					uni.showModal({
+						content: "视频传输错误，请返回重试",
+						showCancel: false,
+						success:(res)=>{
+							this.isErrorModalShowed=false;
+							if (res.confirm) {
+								console.log('用户点击确定');
+							} else if (res.cancel) {
+								console.log('用户点击取消');
+							}
+						}
+					});
+				}
 			},
 			fullScreen(){
 				if(this.videoContext){
